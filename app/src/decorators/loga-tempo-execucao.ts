67de8504ed5dadf6c3e613d6c24e3188f35b15c3
Arthur Vinicius <thuur.vss@gmail.com>
2022-04-23 10:43:10 -0300

    decorator com argumentos

--- app/src/decorators/loga-tempo-execucao.ts
+++ app/src/decorators/loga-tempo-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoExecucao() {
+export function logarTempoExecucao(emSeg: boolean = false) {
     /*
      * Função para logar no console o tempo de execução de uma função por decorators
      */
@@ -10,11 +10,17 @@ export function logarTempoExecucao() {
         // guarda o método original em uma variável
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args: any[]) {
+            let divisor = 1;
+            let unidade = "milisegundos";
+            if (emSeg) {
+                divisor = 1000;
+                unidade = "segundos";
+            }
             const t1 = performance.now()
             // chama o método original e passa os argumentos usando o contexto atual 
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now()
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 100}`)
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidade}`)
             // retorna o valor de retorno de método original
             retorno
         }
