1cb2f3c65cbc2884d440d16b4dd94fee7dc37d1d
Arthur Vinicius <thuur.vss@gmail.com>
2022-04-23 17:13:27 -0300

    Aplicando cache decorator copy

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,7 +1,14 @@
-export function domInjector (seletor:string) {
+export function domInjector(seletor: string) {
     return function (target: any, propertyKey: string) {
+        // para resolver o problema de toda hora o decorator ficar batendo no html
+        // criamos uma varivel que será verificada antes de bater no html
+        let elemento: HTMLElement;
         const getter = function () {
-            const elemento = document.querySelector(seletor);
+            // se a variavel estiver vazia, só ai ele bate no html
+            if (!elemento) {
+                elemento = <HTMLElement>document.querySelector(seletor);
+            }
+            // se ela ja estiver la, ele so retorna
             return elemento;
         }
         Object.defineProperty(
